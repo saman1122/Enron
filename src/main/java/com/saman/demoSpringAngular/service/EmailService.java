a86f9134dfecd271e11945f31b4823704b7781eb
saman1122 <saman11@hotmail.fr>
2018-03-15 11:38:15 +0100

    fix no result when search + Pageable (for search)

--- src/main/java/com/saman/demoSpringAngular/service/EmailService.java
+++ src/main/java/com/saman/demoSpringAngular/service/EmailService.java
@@ -5,12 +5,14 @@ import com.saman.demoSpringAngular.repository.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class EmailService {
@@ -19,18 +21,18 @@ public class EmailService {
     EmailRepository emailRepository;
 
     public List<Email> getAllEmail() {
-        List<Email> retour = new ArrayList<>();
-        emailRepository.findAll().forEach(retour::add);
-        return retour;
+        return StreamSupport.stream(emailRepository.findAll().spliterator(),false).collect(Collectors.toList());
     }
 
     public Page<Email> getEmailFindByTerm(String term, Pageable pageable) {
-        return  new PageImpl<>(emailRepository.findAll(pageable).stream().filter(t->
+        List<Email> filtered = StreamSupport.stream(emailRepository.findAll().spliterator(),false).filter(t->
                 t.to.stream().anyMatch(to -> to.contains(term)) || t.mailbox.contains(term) || t.from.contains(term)
                         || t.date.toString().contains(term) || t.subject.contains(term)
                         || t.content.contains(term) || t.cc.stream().anyMatch(to -> to.contains(term))
                         || t.bcc.stream().anyMatch(to -> to.contains(term)) || t.user.contains(term)
-        ).collect(Collectors.toList()));
+        ).collect(Collectors.toList());
+
+        return new PageImpl<>(filtered,pageable,filtered.size());
     }
 
     public Page<Email> listAllByPage(Pageable pageable){
